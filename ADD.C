120d952ef5f3916b1540a93160923650ca99d49a
patilyash01 <yashpatil131225@gmail.com>
2022-04-23 13:32:08 +0530

    ADDED:[Yash] Program for Addition

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter the 2 Number:");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\n Add is:%d",c);
+	getch();
+}
