e37a462bdd9965616505040d43826a2f4e0e9352
patilyash01 <yashpatil131225@gmail.com>
2022-04-25 13:12:44 +0530

    ADDED:[yash]c basic program for calculatur

---
+++ CALCULAT.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a=5,b=10,c=10,d=0,e=0,f=0;
+	clrscr();
+	c=a+b;
+	printf("\n Addition=%f",c);
+	d=a*b;
+	printf("\n multiplication=%f",d);
+	e=a-b;
+	printf("\n substraction=%f",e);
+	f=a/b;
+	printf("\n divided=%f",f);
+	getch();
+}
